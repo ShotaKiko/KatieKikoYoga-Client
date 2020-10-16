d24bb7e1dfaad8f7f7b4a431348490c45cc2c92b
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-15 19:30:04 -0500

    added nav skeleton along with preliminary logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/App.scss";
+import NavBar from "./NavBar.js";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      hello world
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="navContainer">
+      <div className="logoDiv">
+        <img src="KKYLogo.png" alt="Logo for KatieKikoYoga" />
+      </div>
+      <div className="linksDiv"></div>
+      <div className="socialMediasDiv"></div>
+    </div>
+  );
+}
+
+export default NavBar;
