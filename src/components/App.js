7b6400121ba8685da8b7ad6d8ea96893967708a7
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-11-07 03:11:51 -0600

    added scroll to top function upon site navigation

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../styles/App.scss";
 import { Switch, Route } from "react-router-dom";
 
-
 //Component Imports
 import LandingPage from "./LandingPage.js";
 import NavBar from "./NavBar.js";
@@ -10,11 +9,13 @@ import CalenderPage from "./CalenderPage.js";
 import Classes from "./Classes.js";
 import Contact from "./Contact.js";
 import Footer from "./Footer.js";
+import ScrollToTop from "./ScrollToTop.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ScrollToTop />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/calender" component={CalenderPage} />
