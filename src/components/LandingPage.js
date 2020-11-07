ee34e057b1cdff7e5e601f0b03977eef63841c9a
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-11-07 02:58:13 -0600

    modified landing page text content

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -18,21 +18,26 @@ function LandingPage() {
             benefits of yoga in the comforts of your own home!
           </Typography>
           <Typography variant="body1" gutterBottom>
-            I am yoga instructor who teaches a variety of online yoga classes. I
-            am a registered 223 hour yoga teacher with experience teaching both
-            kid's and adult's classes.I teach four different styles of classes,{" "}
-            <span>
-              Beginner's Yoga, Kid's Yoga, Advanced and Restorative Yoga
-            </span>
-            .
+            I am a yoga instructor who teaches a variety of online yoga styles
+            and flows for beginners to more advanced yogis! I am a registered
+            223 hour yoga teacher with experience teaching both kid's and
+            adult's classes. <br></br> Currently I teach four different styles of classes:<br></br>
           </Typography>
 
           <Typography variant="body1" gutterBottom>
-            My work as a elementary teacher has lead me to be quite the
+            <span>
+              Beginner's Yoga, Kid's Yoga, Advanced and Restorative Yoga
+            </span>
+            <br></br>and I also offer <span>Private Yoga Classes</span>.
+          </Typography>
+
+          <Typography variant="body1" gutterBottom>
+            My work as a elementary teacher has lead me to become quite the
             storyteller but my love for true healing of the mind body and soul
             is what continues to drive and motivate my yoga teachings. I have
-            been doing yoga for the past 16 years as a way for myself to cope
-            with chronic pain and stress.
+            been doing yoga for the past 16 years as a way for myself maintain <span>balance </span>
+            and meditate upon my <span>inner peace</span>.
+            {/* to cope with chronic pain and stress. */}
           </Typography>
         </div>
         <div className="imageDiv">
@@ -41,7 +46,6 @@ function LandingPage() {
             src="gracefulTree.jpg"
             effect="black-and-white"
           />
-          {/* <img src="gracefulTree.jpg" alt="Katie Kiko" /> */}
         </div>
       </div>
     </div>
